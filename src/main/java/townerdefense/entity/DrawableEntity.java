fe75be61ba57f01f521bf085f241595d79c9dfe5
Bien <bien.uet279@gmail.com>
2019-11-10 12:25:05 +0700

    Optimize code

--- src/main/java/townerdefense/entity/DrawableEntity.java
+++ src/main/java/townerdefense/entity/DrawableEntity.java
@@ -1,7 +1,6 @@
 package townerdefense.entity;
 
 import javafx.scene.canvas.GraphicsContext;
-import townerdefense.entity.GameEntity;
 
 public interface DrawableEntity {
     public void render(GraphicsContext graphicsContext);
--- src/main/java/townerdefense/entity/MovableEntity.java
+++ src/main/java/townerdefense/entity/MovableEntity.java
@@ -1,7 +1,5 @@
 package townerdefense.entity;
 
-import townerdefense.entity.other.Point;
-
 public interface MovableEntity {
 
 }
