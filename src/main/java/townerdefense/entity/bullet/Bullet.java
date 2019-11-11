5a9e882e3f621260172a870988f9b063c490bb1e
Quang Dinh <ngquandinhhzz@gmail.com>
2019-11-11 16:13:54 +0700

    update destroy bullet

--- src/main/java/townerdefense/entity/bullet/Bullet.java
+++ src/main/java/townerdefense/entity/bullet/Bullet.java
@@ -3,21 +3,28 @@ package townerdefense.entity.bullet;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import townerdefense.GameConfig;
+import townerdefense.entity.DestroyableEntity;
 import townerdefense.entity.Entity;
 import townerdefense.entity.UpdateableEntity;
 import townerdefense.entity.enemy.Enemy;
 
-public class Bullet extends Entity implements UpdateableEntity {
+public class Bullet extends Entity implements UpdateableEntity, DestroyableEntity {
     public boolean isCanRemove = false;
     private double speed;
     private double damage;
     private Enemy enemy;
+    private double deltaDistance;
+    private double deltaX;
+    private double deltaY;
+    private double theta;
+    private double Distance;
 
     public Bullet(Enemy enemy, double posX, double posY, double with, double height, double speed, double damage) {
         super(posX, posY, with, height);
         this.speed = speed;
         this.damage = damage;
         this.enemy = enemy;
+
     }
 
     public Bullet(Enemy enemy, double posX, double posY, double damage) {
@@ -35,11 +42,17 @@ public class Bullet extends Entity implements UpdateableEntity {
 
     @Override
     public void update(int deltaTime) {
-        final double deltaDistance = this.speed * deltaTime / GameConfig.NPS;
-        final double deltaX = enemy.getCenterPosX() - this.posX;
-        final double deltaY = enemy.getCenterPosY() - this.posY;
-        final double theta = Math.atan2(deltaY, deltaX);
+         deltaDistance = this.speed * deltaTime / GameConfig.NPS;
+         deltaX = enemy.getCenterPosX() - this.posX;
+         deltaY = enemy.getCenterPosY() - this.posY;
+         theta = Math.atan2(deltaY, deltaX);
         this.posX += deltaDistance * Math.cos(theta);
         this.posY += deltaDistance * Math.sin(theta);
+        Distance = Math.sqrt(Math.pow(enemy.getCenterPosX()-this.getCenterPosX(),2) + Math.pow(enemy.getCenterPosY()-this.getCenterPosY(),2) );
+    }
+
+    @Override
+    public boolean onDestroy() {
+        return this.Distance <= 10;
     }
 }
